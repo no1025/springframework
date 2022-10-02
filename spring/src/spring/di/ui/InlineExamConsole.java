3f60598ac2150f031e862e48c27bb928e4e3e6c1
no1025 <shgmlwn1234@naver.com>
2022-10-02 20:45:11 +0900

    스프링프레임워크 학습

---
+++ spring/src/spring/di/ui/InlineExamConsole.java
@@ -0,0 +1,35 @@
+package spring.di.ui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import spring.di.entity.Exam;
+
+@Service
+public class InlineExamConsole implements ExamConsole {
+	
+	private Exam exam;
+	
+	public InlineExamConsole() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public InlineExamConsole(Exam exam) {
+		this.exam = exam;
+	}
+
+	@Override
+	public void print() {
+		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg() );
+
+	}
+	
+	@Autowired
+	@Qualifier("exam1")
+	@Override
+	public void setExam(Exam exam) {
+		this.exam = exam;
+	}
+
+}
